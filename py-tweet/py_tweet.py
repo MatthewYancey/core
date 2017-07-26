af81b63539daa2e636fbbb900d45195864316c79
Matt Yancey <matthewyancey@gmail.com>
2017-07-26 12:33:17 -0400

    corrected

--- py-tweet/py_tweet.py
+++ py-tweet/py_tweet.py
@@ -1,13 +1,15 @@
 from twitter import *
+from time import gmtime, strftime
 
-# class direct_message(object):
-#
-#     def __init__(self):
+class direct_message(object):
 
-access_token = '727068165067264001-6txuLk1EPrSCpIThbnEyvIYMpxKHHw6'
-access_token_secret = 'bQjv2x1HgXcRH8CioXNsIGu72KbI5too6enWDTI4Ldy9i'
-consumer_key = 'vTfFOFQodNkWAZfX352WkXOAD'
-consumer_secret = 'BA2AR5vD0IUHp8cGppGMkOe9zuqUuvdqhIvcohEauyx3r2ZpHF'
-t = Twitter(auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
+    def __init__(self, msg):
 
-t.statuses.update(status = 'test 004')
+        access_token = '727068165067264001-6txuLk1EPrSCpIThbnEyvIYMpxKHHw6'
+        access_token_secret = 'bQjv2x1HgXcRH8CioXNsIGu72KbI5too6enWDTI4Ldy9i'
+        consumer_key = 'vTfFOFQodNkWAZfX352WkXOAD'
+        consumer_secret = 'BA2AR5vD0IUHp8cGppGMkOe9zuqUuvdqhIvcohEauyx3r2ZpHF'
+        t = Twitter(auth = OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
+
+        tweet_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        t.statuses.update(status = msg + ' ' + tweet_time)
