e99313cdd8ee5656005fa0e840e306f4a6ae3dcb
Matt Yancey <matthewyancey@gmail.com>
2019-07-09 10:16:17 -0400

    updated ix to loc

--- data_processing/process_one_hot_encoding.py
+++ data_processing/process_one_hot_encoding.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 # does just what it says
 def one_hot_encoding(df):
-    cats = df.ix[:, df.dtypes == object]
+    cats = df.loc[:, df.dtypes == object]
     one_hot = pd.get_dummies(cats)
-    df = df.ix[:, df.dtypes != object]
+    df = df.loc[:, df.dtypes != object]
     df = pd.concat([df, one_hot], axis = 1)
 
     return df
